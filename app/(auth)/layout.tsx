a06c8f352728f88e49e9ba5b590f04d4995694da
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-11 12:28:01 -0500

    updtae

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+export const metadata = {
+      title: 'Threads',
+      description: 'A Next.js 13 Meta Threads Application'
+}
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+       children 
+      } : { 
+            children: React.ReactNode 
+      }) {
+            return (
+                  <ClerkProvider>
+                        <html lang="eng">
+                              <body className={inter.className}>
+
+                              </body>
+
+                        </html>
+                  </ClerkProvider>
+            )
+      }
