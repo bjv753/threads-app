4adf7fc711acd1125d6eba8d82d5b4112898a411
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-11 16:55:38 -0500

    update

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 function Topbar() {
-      return <h1>Topbar</h1>
+      return (
+      <nav className="topbar">
+            <link href="/"  className="flex 
+            items-center gap-4">
+                  <Image src="/logo.svg" alt="logo" width={28} height={28} />
+                  <p className="text-heading3-bold text-light-1" ></p>
+            </link>
+      </nav>
+      )
 }
 
 export default Topbar;
