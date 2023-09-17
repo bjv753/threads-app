c54138da45ba0f20ff3882ab8cf8ae0274793d9c
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-17 15:45:37 -0500

    update

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -36,8 +36,8 @@ function PostThread({ userId }: {userId: string}) {
               }
         });
 
-      const onSubmit = () => {
-            
+      const onSubmit = async () => {
+            // await createThread()
       }
 
       return (
