6c2e44bf5763eb0832d8bf4cc580e51ea6b2792a
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-18 18:55:26 -0500

    update

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -33,7 +33,7 @@ createdAt,
 comments
 }: Props) => {
       return (
-            <article>
+            <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7" >
                   <h2 className="text-small-regular text-light-2">
                         {content}
                   </h2>
