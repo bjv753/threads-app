af7d8eea6ce3eb3a3035a7b08bb72bcd19637a6b
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-22 16:04:51 -0500

    update

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -27,8 +27,13 @@ const ProfileHeader = ({ accountId, authUserId, name, username, imgUrl, bio }: P
                                           <p className="text-base-medium text-gray-1">@{username}</p>
                                     </div>
                               </div>
-                        </div>
                   </div>
+
+                              {/* TODO: community */}
+
+                              <p className="mt-6 max-w-lg text-base-regular text-light-2" >{bio}</p>
+                              <div className="mt-12 h-0.5 w-full bg-dark-3" />
+                        </div>
 
       )
 }
