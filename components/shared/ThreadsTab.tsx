3f04066245392affc9b1884a73414c7912181237
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-22 17:06:31 -0500

    update

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,35 @@
+import { fetchUserPosts } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+
+interface Props {
+      currentUserId: string;
+      accountId: string;
+      accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+
+      let result = await fetchUserPosts(accountId);
+
+      if(!result) redirect('/')
+
+      return (
+            <section className="mt-9 flex flex-col gap-10">
+                  {result.threads.map((thread) => (
+                        <ThreadCard 
+                        key={thread._id}
+                        id={thread._id}
+                        currentUserId={user?.id || " "}
+                        parentId={thread.parentId}
+                        content={thread.text}
+                        author={thread.author}
+                        community={thread.community}
+                        createdAt={thread.createdAt}
+                        comments={thread.children}
+                 />
+                  ))}
+            </section>
+      )
+}
+
+export default ThreadsTab;
