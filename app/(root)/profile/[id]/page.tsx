e822df5799496090151a5354fd8337bcf66ef8a2
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-22 15:34:28 -0500

    update

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,22 +1,28 @@
+import ProfileHeader from '@/components/shared/ProfileHeader';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
-async function Page({ params: { id: string } }) {
+async function Page({ params }: { params: { id: string }}) {
       const user = await currentUser();
-      
       if(!user) return null;
 
-      const userInfo = await fetchUser(user.id);
-
+      const userInfo = await fetchUser(params.id);
       if(!userInfo?.onboarded)  redirect('/onboarding');
 
-const Page = async () => {
       return (
             <section>
-                  Profile
+                  <ProfileHeader
+                        accountId={userInfo.id}
+                        authUserId={user.id}
+                        name={userInfo.name}
+                        username={userInfo.username}
+                        imgUrl={userInfo.image}
+                        bio={userInfo.bio}
+                  />
             </section>
       )
 }
 
-export default Page;
+
+export default Page
